c6459398bbd167b1854040f8cbcfe5e91d7ef356
Mumin Bashir <moominbashir732@gmail.com>
2023-06-17 20:21:33 +0530

    added db models and routes

---
+++ models/Notes.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  tag: { type: String, deafult: "General" },
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("notes", NotesSchema);
---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+
+// Create a user using POST : (/api/auth/)
+router.post("/", (req, res) => {
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
+});
+
+module.exports = router;
