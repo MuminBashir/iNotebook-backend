8ac2d667c0af54f61c13fb912785d11d239dd00c
Mumin Bashir <moominbashir732@gmail.com>
2023-06-17 23:32:34 +0530

    improving email duplication detection

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
-// Create a user using POST : (/api/auth/)
+// Create a user using POST : (/api/auth/createuser) - no login req
 router.post(
-  "/",
+  "/createuser",
   [
     check("email", "Enter a valid email").isEmail(),
     check("name", "Enter a valid name").isLength({ min: 3 }),
@@ -13,19 +13,32 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.status(200).json(user))
-      .catch((err) => res.status(400).json({ error: err.message }));
+    // Try to create a user and stroe data in database if no error found
+    try {
+      // To check if a user with email already exists
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      // Storing data in database
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Unexpected error occured");
+    }
   }
 );
 
