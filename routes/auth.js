3ade5aa7b5ee946b4962965c37b505f3d284e0e6
Mumin Bashir <moominbashir732@gmail.com>
2023-06-17 22:51:09 +0530

    setup express validator in auth.js route

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 // Create a user using POST : (/api/auth/)
-router.post("/", (req, res) => {
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    check("email", "Enter a valid email").isEmail(),
+    check("name", "Enter a valid name").isLength({ min: 3 }),
+    check("password", "password should contain atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.status(200).json(user))
+      .catch((err) => res.status(400).json({ error: err.message }));
+  }
+);
 
 module.exports = router;
