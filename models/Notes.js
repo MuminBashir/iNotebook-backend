dbd52790e14b301df4116aa7321d24f281e266e5
Mumin Bashir <moominbashir732@gmail.com>
2023-06-18 22:48:24 +0530

    created endpoint for updating notes in notes.js route

--- models/Notes.js
+++ models/Notes.js
@@ -5,7 +5,7 @@ const NotesSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  tag: { type: String, deafult: "General" },
+  tag: { type: String, default: "General" },
   date: { type: Date, default: Date.now },
 });
 
--- routes/notes.js
+++ routes/notes.js
@@ -16,9 +16,9 @@ router.get("/getallnotes", fetchuser, async (req, res) => {
   }
 });
 
-// Route 2 :: Add notes using: POST "/api/notes/addnotes" - login req
+// Route 2 :: Add notes using: POST "/api/notes/addnote" - login req
 router.post(
-  "/addnotes",
+  "/addnote",
   [
     check("title", "Title can't be blank").exists(),
     check("description", "Description can't be blank").exists(),
@@ -46,4 +46,44 @@ router.post(
     }
   }
 );
+
+// Route 3 :: Update existing note using: PUT "/api/notes/updatenote/:id" - login req
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+
+    // create new note
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    //find note to update and update it
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send({ error: "Note not found" });
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Access Denied");
+    }
+
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true } 
+    );
+    res.status(200).json(note) 
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Unexpected error occured");
+  }
+});
+
 module.exports = router;
